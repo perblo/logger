54faf63993def6afeddffb26211ee2dd1c69e608
Per Blommegård <per@blommegard.se>
2012-10-29 15:15:33 +0100

    Revert "new webtools with energy measurement"
    This reverts commit 87914ecf5adde16b60e78c8d33f96242a4108ed2.

--- update_db.php
+++ update_db.php
@@ -1,5 +1,5 @@
 <?php
-//echo ("update_db.php indicator");
+
 
 function mysql_insert($table, $inserts) {
 echo $table;
@@ -19,33 +19,24 @@ if(!$con)
 	{
 	die('Could not connect: ' .mysql_error());
 	}
-$db = mysql_select_db('database', $con);
+$db = mysql_select_db('storgatan69', $con);
 echo "inne";
 
-if (isset($_GET['T1'])){
-	mysql_insert('Temp', array(
-    	'Time' => $_GET['Time'],
-    	't1' => $_GET['T1']/100,
-    	't2' => $_GET['T2']/100,
-    	't3' => $_GET['T3']/100,
-    	't4' => $_GET['T4']/100
-   	));
-}
-if (isset($_GET['H1'])){
-	mysql_insert('Humidity', array(
-	    'Time' => $_GET['Time'],
-	    'h1' => $_GET['H1']/100,
-	    'h2' => $_GET['H2']/100,
-	    'h3' => $_GET['H3']/100,
-	    'h4' => $_GET['H4']/100
-	    ));
-}
+mysql_insert('Temp', array(
+    'Time' => $_GET['Time'],
+    't1' => $_GET['T1']/100,
+    't2' => $_GET['T2']/100,
+    't3' => $_GET['T3']/100,
+    't4' => $_GET['T4']/100
+    ));
 
-if (isset($_GET['E'])){
-	mysql_insert('Energy', array(
-	    'Time' => $_GET['Time'],
-	    'E' => $_GET['E']
-	    ));
-}
+mysql_insert('Humidity', array(
+    'Time' => $_GET['Time'],
+    'h1' => $_GET['H1']/100,
+    'h2' => $_GET['H2']/100,
+    'h3' => $_GET['H3']/100,
+    'h4' => $_GET['H4']/100
+    ));
+
 mysql_close($con);
 ?> 
